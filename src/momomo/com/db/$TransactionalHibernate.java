9c7c26b7f4aec6e3cbaa4058cc5ae02fdfc96dd9
momomo <opensource@momomo.com>
2021-04-07 16:50:22 +0200

    Empty

--- src/momomo/com/db/$TransactionalHibernate.java
+++ src/momomo/com/db/$TransactionalHibernate.java
@@ -1,13 +1,13 @@
 package momomo.com.db;
 
 import momomo.com.annotations.informative.Protected;
-import momomo.com.db.sessionfactory.$SessionFactoryNewRequireHibernate;
+import momomo.com.db.sessionmanager.$SessionManagerNewRequire;
 import org.hibernate.Session;
 
 /**
  * @author Joseph S.
  */
-public interface $TransactionalHibernate extends $Transactional<$TransactionHibernate, $TransactionOptionsHibernate>, $SessionFactoryNewRequireHibernate {
+public interface $TransactionalHibernate extends $Transactional<$TransactionHibernate, $TransactionOptionsHibernate>, $SessionManagerNewRequire {
     
     @Protected default $TransactionManagerHibernate transactionManager() {
         return transactionManager(session());
